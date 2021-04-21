ff4dac31b511c912b81c3922ebbb330bbba106e5
Carletta Walls <carlettawalls88@gmail.com>
2021-04-21 18:18:10 -0400

    updated css, html, and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+var scores = document.querySelector("#scores")
+var timer = document.querySelector("#timer")
+var instructions = document.querySelector(".instructions")
+var startButton = document.querySelector(".start-button")
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+function startGame() {
+
+}
+
+startButton.addEventListener("click", startGame);
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// Slide 1
+// Coding Quiz Challenge
+// Try to answer the following questions within the time
+// Limit. Keep in mind the incorrect answers will penalize your score-time
+// By ten-seconds!
+// Slide 2
+// Question #1
+// Commonly used Data Types do not include:
+// 1.Strings
+// 2.Booleans
+//  3. Alerts
+//  4. Numbers
+// Slide 3
+// Question #2
+// The condition in an if/else statement is enclosed within _______.
+// 1.      Quotes
+// 2.      Curly brackets
+// 3.      Parenthesis
+// 4.      Square brackets
+// Slide 4
+// Question #3
+// Arrays in JavaScript can be used to store _____.
+// 1.      Numbers and strings
+// 2.      Other arrays
+// 3.      Booleans
+// 4.      All the above
+ 
+// Slide 5
+// Question #4
+// String values must be enclosed within _______
+// when being assigned to variables.
+// 1.      Commas
+// 2.      Curly brackets
+// 3.      Quotes
+// 4.      Parenthesis
+// Slide 6
+// Question #5
+// A very useful tool used during development
+// and debugging for printing content to the
+// debugger is:
+// 1.      JavaScript
+// 2.      Terminal/bash
+// 3.      For loops
+// 4.      Console.log
+// Slide 7
+// All done!
+// Your final score is ___.
+// Enter initials: _____             Submit Button
+// Slide 8
+ 
+// Highscores list
+// Go back button                 Clear Highscores button
