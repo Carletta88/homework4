dee2e68dd1f13c6416548881337d8cbc0218ad9b
Carletta Walls <carlettawalls88@gmail.com>
2021-04-24 17:28:03 -0400

    updated the javascript file with the check function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ var quiz = document.querySelector('.class')
 var startButton = document.querySelector(".start-button")
 var results = document.querySelector('.results')
 var index = 0;
-var correct = 0;
-var choice = 0;
+let userAnswers = new Array(5);
+var correctAnswers = ["C", "C", "D", "C", "D"];
 var questions = [
     {
         question: 'Commonly used Data Types do not include:',
@@ -49,7 +49,7 @@ var questions = [
         D: 'Console.log',
         Answer: 'Console.log'
         
-    },
+    }
     
 ]
  
@@ -87,26 +87,28 @@ startButton.addEventListener("click", startTimer);
 
 function displayQuestion(questionIndex)
 {
-    // checkAnswer();
-
     var title = document.createElement("h3")
     title.textContent = questions[questionIndex].question
 
     var A = document.createElement("button");
     A.classList.add("options");
     A.textContent = questions[questionIndex].A;
-    
+    A.onclick = function() {userAnswers[questionIndex] = "A"};
+        
     var B = document.createElement("button");
     B.classList.add("options");
     B.textContent = questions[questionIndex].B;
+    B.onclick = function() {userAnswers[questionIndex] = "B"};
 
     var C = document.createElement("button");
     C.classList.add("options");
     C.textContent = questions[questionIndex].C
+    C.onclick = function() {userAnswers[questionIndex] = "C"};
 
     var D = document.createElement("button");
     D.classList.add("options");
     D.textContent = questions[questionIndex].D
+    D.onclick = function() {userAnswers[questionIndex] = "D"};
 
     document.getElementById("data").append(title)
     document.getElementById("data").append(A)
@@ -120,40 +122,24 @@ function displayQuestion(questionIndex)
     for(var i = 0; i < allButtons.length; i++){
         allButtons[i].addEventListener('click', function(){
             document.getElementById("data").textContent = ""
-       displayQuestion(index)
+            if(index < 5 ) {
+               displayQuestion(index)
+        }
         })
     }
-
+    
 }
 
-// function checkAnswer(choice){
-    //     var allButtons = document.getElementsByClassName("options");
-    //     if(choice == questions[questionIndex].question.answer){
-        //         correct++;
-        //         console.log("RIght")
-        //     }
-        
-        // }
-        
-        // allButtons.addEventListener('click', checkAnswer);
+function checkAnswers() {
+    for (var i = 0; i < correctAnswers.length; i++) {
+        if (userAnswers[i] === correctAnswers[i]) {
+              correct++;
+        }
+    }
+ }
 
+ checkAnswers();
 
-    
-    
-    
-
-
-// When the user clicks on the element display the element that is clicked on, we want to be able to return the text from a element after clicking on it. Call the function inside the display function.
-
-// THEN a timer starts and I am presented with a question -DONE
-// WHEN I answer a question
-// THEN I am presented with another question -DONE
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
 
 
 // Slide 7
